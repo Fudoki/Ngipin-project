8b48d07d1a7b2c75bfee43b002eac9476053feb4
Fudoki <133984673+Fudoki@users.noreply.github.com>
2023-05-19 01:59:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,62 +2,62 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-}
+if (menu && navbar) {
+  menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+  };
 
-window.onscroll = () => {
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
+  window.onscroll = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+  };
 }
 
 function logOut() {
-  window.location.assign("index.html")
+  window.location.assign("index.html");
   alert('You logged out successfully..');
 }
 
-
-
 // Register an account
+var registrationForm = document.getElementById('form-box');
+if (registrationForm) {
+  registrationForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
 
-document.getElementById('form-box').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form submission
+    // Get input values
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 
-  // Get input values
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
+    // Create user object
+    var user = {
+      username: username,
+      password: password
+    };
 
-  // Create user object
-  var user = {
-    username: username,
-    password: password
-  };
+    // Check if local storage already has registered users
+    var registeredUsers = localStorage.getItem('registeredUsers');
+    if (registeredUsers) {
+      registeredUsers = JSON.parse(registeredUsers); // Parse existing data
+      registeredUsers.push(user); // Add new user to array
+      localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers)); // Update local storage
+    } else {
+      localStorage.setItem('registeredUsers', JSON.stringify([user])); // Create new array with user
+    }
 
-  // Check if local storage already has registered users
-  var registeredUsers = localStorage.getItem('registeredUsers');
-  if (registeredUsers) {
-    registeredUsers = JSON.parse(registeredUsers); // Parse existing data
-    registeredUsers.push(user); // Add new user to array
-    localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers)); // Update local storage
-  } else {
-    localStorage.setItem('registeredUsers', JSON.stringify([user])); // Create new array with user
-  }
+    // Save the currently registered user for logging in
+    localStorage.setItem('currentUser', JSON.stringify(user));
 
-  // Save the currently registered user for logging in
-  localStorage.setItem('currentUser', JSON.stringify(user));
-  
-  alert('Registration successful!');
-  document.getElementById('registrationForm').reset(); // Reset the form
-});
+    alert('Registration successful!');
+    document.getElementById('form-box').reset(); // Reset the form
+  });
+}
 
-
-
-
-function appointment() {
-  document.addEventListener('DOMContentLoaded', function() {
-    var form = document.getElementById('appointmentForm');
-    form.addEventListener('submit', function(event) {
+// Appointment booking
+document.addEventListener('DOMContentLoaded', function() {
+  var appointmentForm = document.getElementById('appointmentForm');
+  if (appointmentForm) {
+    appointmentForm.addEventListener('submit', function(event) {
       event.preventDefault(); // Prevent form submission
 
       var fullName = document.getElementById('fullName').value;
@@ -100,7 +100,7 @@ function appointment() {
 
           alert('Appointment booked successfully!');
           // Reset the form
-          form.reset();
+          appointmentForm.reset();
         } else {
           alert('Please select a future date and time for the appointment.');
         }
@@ -108,7 +108,5 @@ function appointment() {
         alert('Please fill in all the required fields.');
       }
     });
-  });
-}
-
-
+  }
+});
